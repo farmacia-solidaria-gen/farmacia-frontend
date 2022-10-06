db4a1f3a14a511a86c016e252fcb149f29909b46
Pamela Lima <pameladelima@gmail.com>
2022-10-06 09:59:00 -0300

    Postagem

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { Box, Button, Grid, Typography } from '@mui/material';
 import './Home.css';
+import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 
 function Home() {
   return (
@@ -20,6 +22,7 @@ function Home() {
           <img src="https://thumbs.dreamstime.com/b/farmaceuta-wektoru-ilustracja-farmaceuta-na-tle-p%C3%B3%C5%82ki-z-lekarstwami-71719701.jpg" alt="" width="600px" height="500px" />
         </Grid>
         <Grid xs={12} style={{ backgroundColor: "white" }}>
+        <TabPostagem/>
         </Grid>
       </Grid>
     </>
