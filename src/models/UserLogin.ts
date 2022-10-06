deaf9cf5de7ac015ddb3000b95fd3486317487d0
Maikon-Santos <maikoon-santoz@gmail.com>
2022-10-06 11:29:38 -0300

    componente login

---
+++ src/models/UserLogin.ts
@@ -0,0 +1,16 @@
+interface UserLogin{
+    id: number;
+    nome: string;
+    usuario: string;
+    senha: string;
+    foto: string;
+    CPF: number;
+    endereco: string
+    token: string
+    
+    
+
+}
+export default UserLogin;
+
+
