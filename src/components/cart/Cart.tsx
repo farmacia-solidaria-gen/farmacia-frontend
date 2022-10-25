b157e3d17b9295b83d58b4913d4b63aec1efde89
veronica pupo <veronica.pupo@hotmail.com>
2022-10-25 10:26:46 -0300

    cart

---
+++ src/components/cart/Cart.tsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useState, ChangeEvent } from 'react'
+import { Box, Button, Card, TextField, Typography } from '@mui/material'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+
+import Produto from '../../models/Produto'
+import { buscaId } from '../../service/Service'
+
+import './Cart.css'
+import { useSelector } from 'react-redux'
+import { TokenState } from '../../store/tokens/tokensReducer'
+import { toast } from 'react-toastify'
+
+
+
+function Cart() {
+   
+
+    let history = useNavigate()
+
+    const { id } = useParams<{ id: string }>()
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    const [quantidadeFinal, setQuantidadeFinal] = useState(0)
+
+    const [produto, setProduto] = useState<Produto>({
+        id: 0,
+        nome: '',
+        descricao: '',
+        fabricante: '',
+        quantidade: 0,
+        preco: 0,
+        categoria: null,
+        usuario: null 
+    
+    })
+
+    useEffect(() => {
+        if (token === "") {
+            toast.error('Você precisa estar logado!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+            history("/login")
+        }
+    }, [token])
+
+     useEffect(() => {
+         if (id !== undefined) {
+            findByIdProduto(id)
+         }
+     }, [id])
+
+     async function findByIdProduto(id: string) {
+     await buscaId(`produto/${id}`, setProduto, {
+            headers: {
+                 'Authorization': token
+            }
+         })
+     }
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        let valor = +e.target.value
+        setQuantidadeFinal(valor);
+    }
+
+    function valorTotal() {
+        return quantidadeFinal * produto.preco
+    }
+
+    
+    function confirmSales() {
+        toast.success('Compra confirmada, para os próximos passos confirme seu e-mail! ✔', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
+        history("/carrinho")
+    }
+
+    function cancelar() {
+        history("/produtos")
+      }
+    return (
+        <>
+            <Box m={2} display="flex" justifyContent="center" className='caixaCart'>
+                <Card variant="outlined" className='cartContainer'>
+
+                    <div className='cardProduct'>
+                        <img src={produto.fabricante} alt="Img" width= '250px' />
+
+                        <div className='cardProductInfo'>
+                            <Typography color="textSecondary" gutterBottom>
+                                Produto
+                            </Typography>
+
+                            <Typography variant="h5" component="h2">
+                                {produto.nome}
+                            </Typography>
+
+                            <Typography variant="body2" component="p">
+                            {new Intl.NumberFormat('pt-BR',{
+                        style: 'currency',
+                        currency: 'BRL',
+                      }).format(produto.preco)
+                     }
+                            </Typography>
+
+                            <Typography variant="body2" component="p">
+                                Quantidade Máx: {produto.quantidade}
+                            </Typography>
+
+                            <TextField
+                                value={quantidadeFinal}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
+                                InputProps={{ inputProps: { min: 1, max: produto.quantidade } }}
+                                id="quantidade" label="quantidade" type="number" variant="outlined"
+                                name="quantidade" margin="normal" fullWidth
+                            />
+
+                            <Typography variant="body2" component="p">
+                                Total: R$ {valorTotal()}
+                            </Typography>
+                        </div>
+                    </div>
+
+                </Card>
+
+                <Box display="flex" flexDirection="column" justifyContent="center" mb={1.5}>
+
+                    <Box className="cardProductButton">
+                        <Box mx={1} >
+                            <Button onClick={confirmSales} variant="contained" size='small' color="primary" className='botaoCompra'>
+                                Confimar Compra
+                            </Button>
+                        </Box>
+                    </Box>
+
+                    <Link to="/produtos" className="cardProductButton">
+                        <Box mx={1}>
+                            <Button onClick={cancelar} variant="contained" size='small' color="secondary" className='botaoCancelarCompra'>
+                                Cancelar
+                            </Button>
+                        </Box>
+                    </Link>
+
+                </Box>
+            </Box>
+        </>
+    )
+}
+
+export default Cart
--- src/components/perfil/Perfil.tsx
+++ src/components/perfil/Perfil.tsx
@@ -5,6 +5,9 @@ import User from '../../models/User'
 import { buscaId } from '../../service/Service'
 import { TokenState } from '../../store/tokens/tokensReducer'
 import './Perfil.css'
+import { Box, CardContent, CardMedia } from '@mui/material';
+import { Link} from 'react-router-dom'
+import { makeStyles, Card, CardActions,  Button, InputAdornment } from '@material-ui/core';
 
 
 function Perfil() {
@@ -53,11 +56,40 @@ function Perfil() {
 
             <div className="postUser">
             {usuario.produto?.map((produtos) => (
+              
               <div className="postPerfil">
+                           
                 <h3>{produtos.nome}</h3>
+                <CardMedia
+                    component="img"
+                    height="250"
+                    image={produtos.fabricante}
+
+                    //className={classes.media}
+                    
+                     />
+<Box display="flex" justifyContent="center" mb={1.5}>
+<Link to={`/formularioProduto/${produtos.id}`} className="text-decorator-none" >
+                    <Box mx={1}>
+                      <Button variant="contained" className="botao-atualizar" size='small' color="primary" >
+                      Atualizar 
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarProduto/${produtos.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" className="botao-deletar" size='small' color="secondary">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
+                  </Box>
                 <strong className='txt'>Categoria: {produtos.categoria?.tipo}</strong>
-                <p>{produtos.quantidade}</p> 
+                <p>Quantidade:{produtos.quantidade}</p> 
+                
               </div>
+
+
             ))}
             </div>
           </Grid>
